2c6dfe4e95574941641e86efb9c10dd7f34fc674
rantnar <kulinski90@gmail.com>
2024-10-31 11:21:28 +0100

    poprawki jezykowe w init, dodano mozliwosc wylaczania/wlaczania podkreslania slow kluczowych

--- init.lua
+++ init.lua
@@ -39,19 +39,22 @@ function chaosforge:config_cheatsheet()
     cecho("=== ⚙  ==============================================================\n")
 
     cecho(" " .. string.sub(string.format("%-40s", "chaosforge.longs"),1,40) .. " " .. "<magenta>" .. string.sub(string.format("%-10s", tostring(chaosforge.longs)),1,10) .. "<reset>")
-    cecho(" - czy wyswietlac dlugi opis lokacji jako okno\n")
+    cecho(" - wyswietla opis lokacji w oknie\n")
+
+    cecho(" " .. string.sub(string.format("%-40s", "chaosforge.longs_patterns"),1,40) .. " " .. "<magenta>" .. string.sub(string.format("%-10s", tostring(chaosforge.longs_patterns)),1,10) .. "<reset>")
+    cecho(" - wyswietla slowa kluczowe w oknie z opisem lokacji\n")
 
     cecho(" <dim_grey>" .. string.sub(string.format("%-40s", "chaosforge.replacements"),1,40) .. " " .. "<magenta>" .. string.sub(string.format("%-10s", tostring(chaosforge.replacements)),1,10) .. "<reset>")
-    cecho(" <dim_grey>- usuwanie linii dookola tekstu zaslon <red>(restart)<reset>\n")
+    cecho(" <dim_grey>- usuwa linie dookola tekstu zaslon <red>(restart)<reset>\n")
 
     cecho(" " .. string.sub(string.format("%-40s", "chaosforge.item_hints"),1,40) .. " " .. "<magenta>" .. string.sub(string.format("%-10s", tostring(chaosforge.item_hints)),1,10) .. "<reset>")
-    cecho(" - wyswietlaj podpowiedzi do przedmiotow <red>(restart)<reset>\n")
+    cecho(" - wyswietla podpowiedzi do przedmiotow <red>(restart)<reset>\n")
 
     cecho(" " .. string.sub(string.format("%-40s", "arkadia_findme.highlight_current_room"),1,40) .. " " .. "<magenta>" .. string.sub(string.format("%-10s", tostring(arkadia_findme.highlight_current_room)),1,10) .. "<reset>")
-    cecho(" - czy kolorowac slady na mapce\n")
+    cecho(" - koloruje slady na mapce\n")
 
     cecho(" " .. string.sub(string.format("%-40s", "arkadia_findme.debug_enabled"),1,40) .. " " .. "<magenta>" .. string.sub(string.format("%-10s", tostring(arkadia_findme.debug_enabled)),1,10) .. "<reset>")
-    cecho(" - wyswietlaj wyniki wyszukiwania pokojow\n")
+    cecho(" - wyswietla wyniki wyszukiwania pokojow\n")
 
     cecho(" " .. string.sub(string.format("%-40s", "arkadia_findme.search_depth"),1,40) .. " " .. "<magenta>" .. string.sub(string.format("%-10s", tostring(arkadia_findme.search_depth)),1,10) .. "<reset>")
     cecho(" - okresla jak gleboko w bazie szukac pokoju\n")
@@ -63,37 +66,37 @@ function chaosforge:config_cheatsheet()
     cecho(" - rozszerzony tekst zazycia ziola na opis efektu\n")
 
     cecho(" " .. string.sub(string.format("%-40s", "chaosforge.buffbar_enabled"),1,40) .. " " .. "<magenta>" .. string.sub(string.format("%-10s", tostring(chaosforge.buffbar_enabled)),1,10) .. "<reset>")
-    cecho(" - wyswietlam pasek sterydow <red>(restart)<reset>\n")
+    cecho(" - wyswietla pasek sterydow <red>(restart)<reset>\n")
 
     cecho(" <dim_grey>" .. string.sub(string.format("%-40s", "chaosforge.buffbar_position"),1,40) .. " " .. "<magenta>" .. string.sub(string.format("%-10s", tostring(chaosforge.buffbar_position)),1,10) .. "<reset>")
     cecho(" <dim_grey>- umiejscowienie paska sterydow\n")
 
     cecho(" " .. string.sub(string.format("%-40s", "guardhelper.respect_attack_flags"),1,40) .. " " .. "<magenta>" .. string.sub(string.format("%-10s", tostring(guardhelper.respect_attack_flags)),1,10) .. "<reset>")
-    cecho(" - uzywaj flag AWR przy inteligetnych zaslonach\n")
+    cecho(" - uzywa flagi AWR przy inteligetnych zaslonach\n")
 
     cecho(" " .. string.sub(string.format("%-40s", "guardhelper.cooldown_lock"),1,40) .. " " .. "<magenta>" .. string.sub(string.format("%-10s", tostring(guardhelper.cooldown_lock)),1,10) .. "<reset>")
-    cecho(" - blokuj wyslanie komendy zaslony w czasie cooldownu\n")
+    cecho(" - blokuje wyslanie komendy zaslony w czasie cooldownu\n")
 
     cecho(" " .. string.sub(string.format("%-40s", "guardhelper.show_suggested_target"),1,40) .. " " .. "<magenta>" .. string.sub(string.format("%-10s", tostring(guardhelper.show_suggested_target)),1,10) .. "<reset>")
-    cecho(" - zaznacz sugerowany cel obrony w kondycjach\n")
+    cecho(" - zaznacza sugerowany cel obrony w kondycjach\n")
 
     cecho(" " .. string.sub(string.format("%-40s", "guardhelper.show_most_wounded"),1,40) .. " " .. "<magenta>" .. string.sub(string.format("%-10s", tostring(guardhelper.show_most_wounded)),1,10) .. "<reset>")
-    cecho(" - zaznacz najbardziej poranionego w kondycjach\n")
+    cecho(" - zaznacza najbardziej poranionego w kondycjach\n")
 
     cecho(" " .. string.sub(string.format("%-40s", "guardhelper.show_most_attacked"),1,40) .. " " .. "<magenta>" .. string.sub(string.format("%-10s", tostring(guardhelper.show_most_attacked)),1,10) .. "<reset>")
-    cecho(" - zaznacz najbardziej atakowanego w kondycjach\n")
+    cecho(" - zaznacza najbardziej atakowanego w kondycjach\n")
 
     cecho(" " .. string.sub(string.format("%-40s", "guardhelper.show_guard_status"),1,40) .. " " .. "<magenta>" .. string.sub(string.format("%-10s", tostring(guardhelper.show_guard_status)),1,10) .. "<reset>")
-    cecho(" - wyswietlaj animacje cooldownu zaslony\n")
+    cecho(" - wyswietla animacje cooldownu zaslony\n")
 
     cecho(" " .. string.sub(string.format("%-40s", "guardhelper.targeted_guards"),1,40) .. " " .. "<magenta>" .. string.sub(string.format("%-10s", tostring(guardhelper.targeted_guards)),1,10) .. "<reset>")
-    cecho(" - wlacz obsluge zawodow SC i MC\n")
+    cecho(" - wlacza obsluge zawodow SC i MC\n")
 
     cecho(" <dim_grey>" .. string.sub(string.format("%-40s", "chaosforge.states_in_prompt"),1,40) .. " " .. "<magenta>" .. string.sub(string.format("%-10s", tostring(chaosforge.states_in_prompt)),1,10) .. "<reset>")
-    cecho(" <dim_grey>- przenies opisy kondycji do paska dolnego <red>(restart)<reset>\n")
+    cecho(" <dim_grey>- przenosi opisy kondycji do paska dolnego <red>(restart)<reset>\n")
 
     cecho(" " .. string.sub(string.format("%-40s", "chaosforge.discord_notifier"),1,40) .. " " .. "<magenta>" .. string.sub(string.format("%-10s", tostring(chaosforge.discord_notifier)),1,10) .. "<reset>")
-    cecho(" - wyswietlaj logowanie/wylogowanie na discordzie MC\n")
+    cecho(" - wyswietla logowanie/wylogowanie na discordzie MC\n")
 end
 
 return {
